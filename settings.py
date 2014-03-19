f71f0ba408cc9c4f67a581fd13bf565a962eb59b
vivekananda <t.vivekananda@gmail.com>
2014-03-19 07:13:48 +0530

    removed fb related stuff

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@
 # of "djangoappengine" from this file.
 from djangoappengine.settings_base import *
 
-import os, sys
+import os
 
 # Activate django-dbindexer for the default database
 DATABASES['native'] = DATABASES['default']
@@ -11,12 +11,6 @@ DATABASES['default'] = {'ENGINE': 'dbindexer', 'TARGET': 'native'}
 AUTOLOAD_SITECONF = 'indexes'
 
 SECRET_KEY = '=r-$b*8hglm+858&9t043hlm6-&6-3d3vfc4((7yd0dbrakhvi'
-
-TESTING = 'test' in sys.argv
-
-FACEBOOK_APP_ID = '441646785863774'
-FACEBOOK_APP_SECRET = 'c41135702c2ea76f16675d7524df923a'
-
 
 INSTALLED_APPS = (
     'django.contrib.admin',
@@ -37,24 +31,14 @@ MIDDLEWARE_CLASSES = (
 
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    # Uncomment the next line for simple clickjacking protection:
-    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
     'django.contrib.auth.context_processors.auth',
-    'django.core.context_processors.debug',
-    'django.core.context_processors.i18n',
-    'django.core.context_processors.media',
-    'django.core.context_processors.static',
     'django.core.context_processors.request',
-    'django.contrib.messages.context_processors.messages',
-    'django_facebook.context_processors.facebook',
+    'django.core.context_processors.media',
 )
-
 
 # This test runner captures stdout and associates tracebacks with their
 # corresponding output. Helps a lot with print-debugging.
@@ -62,27 +46,6 @@ TEST_RUNNER = 'djangotoolbox.test.CapturingTestSuiteRunner'
 
 STATIC_URL = '/static/'
 
-
 TEMPLATE_DIRS = (os.path.join(os.path.dirname(__file__), 'templates'),)
-
-# List of callables that know how to import templates from various sources.
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
-)
-
-
-
-AUTHENTICATION_BACKENDS = (
-    'django_facebook.auth_backends.FacebookBackend',
-    'django.contrib.auth.backends.ModelBackend',
-)
-
-
-SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
-
-
-
 
 ROOT_URLCONF = 'urls'
